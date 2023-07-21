fb2b8a89568f9fb2de131a4d64fe049ab3cef13a
Ajay M <ajay@acid-tech.com>
2023-07-21 18:56:28 +0530

    Updated main code logic

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from classes.FakeMailBox import FakeMailBox
 from classes.FakePerson import FakePerson
-from reddit_account import create_account, get_email_verification_link
+from reddit_account import create_account
 
-person = FakePerson()
-mailbox = FakeMailBox(person)
+USERS_COUNT = 100000
 
-# Attach mailbox to user
-person.mailbox = mailbox
+for i in range(USERS_COUNT):
+    print(f"Creating user {i}")
+    person = FakePerson()
+    mailbox = FakeMailBox(person)
 
-
-create_account(person, headless=False)
+    # Attach mailbox to user
+    person.mailbox = mailbox
+    create_account(person, headless=False)
